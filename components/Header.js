8bfe1130a7b0e49972bd79d5f1356db41d72499a
Stedman Halliday <stedman.rh@gmail.com>
2026-01-25 11:19:52 -0800

    Set iOS status bar to default (white/light gray)

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
             <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
 
             <meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+<meta name="apple-mobile-web-app-status-bar-style" content="default" />
             <meta name="apple-mobile-web-app-title" content="hmu" />
             <meta name="theme-color" content="#9333ea" />
             <meta name="format-detection" content="telephone=no" />
