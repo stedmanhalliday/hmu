09436ebe00b3134c763f51a8d822e0a1995dd813
Stedman Halliday <stedman.rh@gmail.com>
2026-01-25 11:38:23 -0800

    refactor: clarify iOS install instructions
    Simplify step text and add troubleshooting guidance for users who can't find the install option.

--- components/InstallModal.js
+++ components/InstallModal.js
@@ -5,10 +5,10 @@ function Instructions({ os }) {
         return (
             <div className="text-base text-slate-600 space-y-3">
                 <ol className="ml-5 list-decimal space-y-3">
-                    <li>Tap the <em className="relative font-medium uppercase tracking-wide pl-5 ml-1 mr-0.5 safariShare">share</em> icon at the bottom of the screen (Safari) or top right in the address bar (Chrome).</li>
-                    <li>Scroll down and tap the <em className="font-medium uppercase tracking-wide">Add to home screen</em> option.</li>
+                    <li>Tap the <em className="relative font-medium uppercase tracking-wide pl-5 ml-1 mr-0.5 safariShare">share</em> icon in Safari or Chrome.</li>
+                    <li>Tap the <em className="font-medium uppercase tracking-wide">Add to home screen</em> option to install.</li>
                 </ol>
-                <p>Make sure you&apos;re using the lastest version of Safari or Chrome.</p>
+                <p>Having trouble? Check inside <code className="px-1 py-0.5 bg-slate-200 rounded text-sm">...</code> More menus, look in the address bar, and scroll down to find the correct options.</p>
             </div>
         );
     } else if (os === "android") {
