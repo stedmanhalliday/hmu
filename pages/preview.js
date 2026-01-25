6da457a3b87e01e43b2db02f34aa2886a0f969f3
Stedman Halliday <stedman.rh@gmail.com>
2026-01-25 09:06:34 -0800

    Refine header spacing in home and preview pages (#29)

--- pages/preview.js
+++ pages/preview.js
@@ -361,7 +361,7 @@ export default function Preview() {
         </div>;
 
     return (
-        <Page className="pt-16 opacity-0"
+        <Page className="pt-8 opacity-0"
             style={loading ? null : { "opacity": 1 }}>
             <nav className="fixed z-10 top-0 w-full p-4 flex justify-between">
                 <TextButton className={styles.home} onClick={home}>Home</TextButton>
