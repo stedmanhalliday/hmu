badbd1a16fde3455c95f139e79797c0eec4fbb6d
Stedman Halliday <stedman.rh@gmail.com>
2026-01-26 21:02:18 -0800

    match EditPane labels to nav typography exactly

--- components/EditPane.js
+++ components/EditPane.js
@@ -37,7 +37,7 @@ export default function EditPane(props) {
                         <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
                     </svg>
                 </button>
-                <span className="text-lg text-slate-600">
+                <span className="text-sm text-slate-600 uppercase tracking-widest leading-3">
                     Edit contact
                 </span>
             </div>
@@ -52,7 +52,7 @@ export default function EditPane(props) {
                         <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372.1 74 321.1 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/>
                     </svg>
                 </button>
-                <span className="text-lg text-slate-600">
+                <span className="text-sm text-slate-600 uppercase tracking-widest leading-3">
                     Edit links
                 </span>
             </div>
@@ -67,7 +67,7 @@ export default function EditPane(props) {
                         <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
                     </svg>
                 </button>
-                <span className="text-lg text-red-600">
+                <span className="text-sm text-red-600 uppercase tracking-widest leading-3">
                     Delete contact
                 </span>
             </div>
